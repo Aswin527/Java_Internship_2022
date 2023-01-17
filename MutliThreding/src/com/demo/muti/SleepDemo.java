a955dcb92c367dd0fa7a27a58ca5b2f655cd3aa7
Aswin Ramesh <aswinramesh527@gmail.com>
2023-01-17 22:54:24 +0530

    Thread

--- MutliThreding/src/com/demo/muti/SleepDemo.java
+++ MutliThreding/src/com/demo/muti/SleepDemo.java
@@ -3,8 +3,26 @@ package com.demo.muti;
 public class SleepDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Thread t=new ThreadDemo();
+		t.start();
+		try {
+		t.sleep(100);
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		for(int i=0;i<20;i++) {
+			System.out.println(i);
+		}
 	}
+}
 
+
+class ThreadDemo extends Thread{
+	@Override
+	public void run() {
+		for(char i='a';i<='z';i++) {
+			System.out.println(i);
+		}
+	}
+	
 }
